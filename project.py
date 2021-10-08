3487180202448c20fb6647e7f941869d22d399d1
eugene09 <umid09tuxtayev.com>
2021-10-08 16:33:17 +0500

    added entering sytem function

--- project.py
+++ project.py
@@ -16,4 +16,24 @@ class User:
         self.max_age = 150
 
         self.entering_system()
+    
+     @staticmethod
+     def init_message():
+     print("""             Welcome
+     Register    [1]
+     Log in      [2]""")
 
+      def entering_system(self):
+        self.clear()
+        self.init_message()
+
+        init_input = input("Enter your option: ").strip()
+        option = ['1', '2']
+        while init_input not in option or not init_input.isnumeric():
+            self.clear()
+            print("Invalid input. Please, try again and enter only options below: ")
+            print(option)
+            init_input = input("Enter your option: ").strip()
+        self.register() if init_input == "1" else self.log_in() 
+
+            
