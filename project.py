04d44b40160bf8a6631657e2ebe34ec56539f6d2
eugene09 <umid09tuxtayev.com>
2021-10-08 16:39:44 +0500

    added registering part

--- project.py
+++ project.py
@@ -34,6 +34,50 @@ class User:
             print("Invalid input. Please, try again and enter only options below: ")
             print(option)
             init_input = input("Enter your option: ").strip()
-        self.register() if init_input == "1" else self.log_in() 
+        self.register() if init_input == "1" else self.log_in()
 
+        def register(self):
+            self.clear()
+            print("Welcome registering part")
+            name = input("Enter your name: ").strip().capitalize()
+            while not name.isalpha():
+                self.clear()
+                print("Invalid input. Try again")
+                name = input("Enter your name: ").strip().capitalize()
+
+            login = input("Enter your login: ").strip()
+            while not login.isalnum() or self.is_login_exists(login):
+                self.clear()
+                print("Invalid input or exist login. Try again")
+                login = input("Enter your login: ").strip()
+
+            password = input("Enter your password(more than 5 characters): ")
+            confirm_pass = input("Confirm your password: ")
+
+            while len(password) == 0 or password != confirm_pass or len(password) < 5:
+                self.clear()
+                print("Invalid password or your password didn't match. Try again")
+                password = input("Enter your password(more than 5 characters): ")
+                confirm_pass = input("Confirm your password: ")
+
+            age = input("Enter your age: ").strip()
+            while not age.isnumeric():
+                print("Please, enter only numbers")
+                age = input("Enter your age: ").strip()
+
+            age = int(age)
+            while age < self.min_age or age > self.max_age:
+                self.clear()
+                print(f"Invalid age. To enter this website your age must be between {self.min_age}-{self.max_age} ages!")
+                age = input("Enter your age: ").strip()
+                age = int(age)
             
+            self.name = name
+            self.login = login
+            self.password = password
+            self.age = age
+            my_data = self.database()
+            my_cursor = my_data.cursor()
+            my_cursor.execute(f"insert into login_info values('{self.name}', '{self.login}', '{self.password}', '{self.age}');")
+            my_data.commit()
+            print("You have entered the system!")
